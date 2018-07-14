6898e9248fb659d4746e7dcb10eaae91f7725047
bemon <grzegorz.ben@coderush.pl>
2018-07-14 12:58:33 +0200

    aurelia app initialzation

--- app/aurelia/src/client/preview/render.js
+++ app/aurelia/src/client/preview/render.js
@@ -1,1 +1,14 @@
- 
+import { bootstrap } from 'aurelia-bootstrapper-webpack';
+
+let app = null;
+
+function renderRoot() {
+  if (app) app = null;
+
+  bootstrap();
+}
+
+export default function render({ showMain }) {
+  showMain();
+  renderRoot();
+}
